f0cf53b70f82ef9a959e6c9ed154d5f8d402bd6c
amoungui <samoungui@gmail.com>
2021-05-03 13:45:51 +0200

    get the value tags: score, runtime, type of film,..

--- script.py
+++ script.py
@@ -1,14 +1,60 @@
 #! /usr/bin/env python3
 # coding: utf-8
-import requests as req
-from bs4 import BeautifulSoup as Soup
+import requests 
+from bs4 import BeautifulSoup 
 
 url = 'https://www.imdb.com/search/title/?at=0&num_votes=5000,&sort=user_rating,desc&start=1&title_type=feature'
 
-res = req.get(url)
+res = requests.get(url)
+
+## @method getvote
+## @param tag: a taget
+## @return vote value
+def getvote(tag):
+    return tag.find('span', attrs={'name':'nv'}).text
+
+## @method getrating
+## @param tag: a taget
+## @return rating(note) value
+def getrating(tag):
+    return tag.find('div', attrs={'class':'ratings-imdb-rating'}).find('strong').text
+
+## @method getruntime
+## @param tag: a taget
+## @return runtime value
+def getruntime(tag):
+    return tag.find('span', attrs={'class':'runtime'}).text
+
+## @method getgross
+## @param tag: a taget
+## @return Gross value
+def getgross(tag):
+    return tag.find('span', attrs={'data-value':'28,341,469'}).text
+
+## @method getfilmname
+## @param tag: a taget
+## @return name of film
+def getfilmname(tag):
+    return tag.find('h3', attrs={'class':'lister-item-header'}).find('a').text
+
+## @method gettype
+## @param tag: a taget
+## @return type of film
+def gettype(tag):
+    return tag.find('span', attrs={'class':'genre'}).text
+
+## @method getscore
+## @param tag: a taget
+## @return film metascore
+def getscore(tag):
+    return tag.find('span', attrs={'class':'metascore'}).text
 
 def main():
-    print(res)
+    if res.ok:
+        soup = BeautifulSoup(res.text, 'html.parser')
+        first_results = soup.find_all('div', attrs={'class':'lister-item-content'}) #lister-item-content
+        
+        print(getscore(first_results[1]))
 
 
 if __name__ == '__main__':
