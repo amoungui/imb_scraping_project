6ee7a9787d1e14fac442ec899a8fcd6d2843f67c
amoungui <samoungui@gmail.com>
2021-05-03 08:02:11 +0200

    correction of the import request to resquests

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 # coding: utf-8
-import requests 
+import requests as req
 from bs4 import BeautifulSoup as Soup
 
 url = 'https://www.imdb.com/search/title/?at=0&num_votes=5000,&sort=user_rating,desc&start=1&title_type=feature'
@@ -13,4 +13,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
+
