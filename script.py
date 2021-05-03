2a1b51a4f3c7515c7d7f8ed74093f030b1b9c9f7
amoungui <samoungui@gmail.com>
2021-05-04 00:01:32 +0200

    construct the pagination links

--- script.py
+++ script.py
@@ -3,10 +3,6 @@
 import requests 
 from bs4 import BeautifulSoup 
 
-url = 'https://www.imdb.com/search/title/?at=0&num_votes=5000,&sort=user_rating,desc&start=1&title_type=feature'
-
-res = requests.get(url)
-
 ## @method getvote
 ## @param tag: a taget
 ## @return vote value
@@ -49,12 +45,38 @@ def gettype(tag):
 def getscore(tag):
     return tag.find('span', attrs={'class':'metascore'}).text
 
+## @method getlink
+## @param tag: a taget
+## @return link of single page
+def getlink(tag):
+    target = tag.find('h3', attrs={'class':'lister-item-header'})
+    a = target.find('a')
+    _link = a['href']
+    return _link
+
+## @method _setlink
+## @param tag: a taget
+## @return link of single page
+def _setlink(url):
+    return 'https://www.imdb.com/'+ str(url)+'?ref_=adv_li_tt'
+
+## @method _setlink
+## @param tag: a taget
+## @return link of single page
+def paginate(index):
+    return 'https://www.imdb.com/search/title/?title_type=feature&num_votes=5000,&sort=user_rating,desc&start='+ str(index)+'&ref_=adv_nxt'
+
+
+url = 'https://www.imdb.com/search/title/?at=0&num_votes=5000,&sort=user_rating,desc&start=1&title_type=feature'
+
+res = requests.get(url)
+
 def main():
     if res.ok:
         soup = BeautifulSoup(res.text, 'html.parser')
         first_results = soup.find_all('div', attrs={'class':'lister-item-content'}) #lister-item-content
         
-        print(getscore(first_results[1]))
+        print(paginate(51))
 
 
 if __name__ == '__main__':
